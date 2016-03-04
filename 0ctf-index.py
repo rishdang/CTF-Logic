eceb115e30fc0676f0857982dc9b438096c879f8
Rishabh Dangwal <admin@theprohack.com>
2016-03-04 15:00:16 +0300

    Create 0ctf-index.py
    0CTF Index file base 64 decode

---
+++ 0ctf-index.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+""" 
+0ctf index page code.
+Rishabh Dangwal
+contact me at admin@theprohack.com
+www.theprohack.com
+April 4 2016
+"""
+import base64
+bdata='emxpYi5kZWNvbXByZXNzKCd4nLPTpgrQsyFdi50uBtDDpg63ndiMwAVwGoPFYEzNNrp6dpjOwVQIVU6WC3CZBtcPAIrcScYnKQ=='
+gdata = base64.b64decode(bdata)
+print d
